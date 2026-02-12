2c740749e4baf1177412eda6d0360c54986f2e92
SeeNot <142214624+SeeNot@users.noreply.github.com>
2026-02-12 20:44:03 +0200

    init

---
+++ src/main/java/com/firsProj/FirstProj/FirstProjApplication.java
@@ -0,0 +1,12 @@
+package com.firsProj.FirstProj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstProjApplication.class, args);
+	}
+}
---
+++ src/test/java/com/firsProj/FirstProj/FirstProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.firsProj.FirstProj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
